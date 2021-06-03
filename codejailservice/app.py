9b0d4c7c1a86107e729bd944ccb60c88abfecdc0
Eric Herrera <eric.herrera@edunext.co>
2021-06-02 22:17:49 -0500

    Adjust code-exec view to work with Flask. Run configure codejail method before importing safe_exec modules. Adjust settings and wsgi files.

--- codejailservice/app.py
+++ codejailservice/app.py
@@ -2,18 +2,43 @@ import os
 import json
 import logging
 
-from codejail.safe_exec import SafeExecException, json_safe
-from codejail.safe_exec import not_safe_exec as codejail_not_safe_exec
-from codejail.safe_exec import safe_exec as codejail_safe_exec
+from codejail import jail_code
+
 from copy import deepcopy
 from flask import Flask, Response, jsonify, request
 
 LOG = logging.getLogger(__name__)
 
 app = Flask(__name__)
-env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+env_config = os.getenv("FLASK_APP_SETTINGS", "codejailservice.config.ProductionConfig")
 app.config.from_object(env_config)
 
+def configure_codejail(app):
+    code_jail_settings = app.config["CODE_JAIL"]
+    python_bin = code_jail_settings.get('python_bin')
+    if python_bin:
+        user = code_jail_settings['user']
+        jail_code.configure("python", python_bin, user=user)
+    limits = code_jail_settings.get('limits', {})
+    for name, value in limits.items():
+        jail_code.set_limit(
+            limit_name=name,
+            value=value,
+        )
+    limit_overrides = code_jail_settings.get('limit_overrides', {})
+    for context, overrides in limit_overrides.items():
+        for name, value in overrides.items():
+            jail_code.override_limit(
+                limit_name=name,
+                value=value,
+                limit_overrides_context=context,
+            )
+
+configure_codejail(app)
+
+from codejail.safe_exec import SafeExecException, json_safe
+from codejail.safe_exec import not_safe_exec as codejail_not_safe_exec
+from codejail.safe_exec import safe_exec as codejail_safe_exec
 
 @app.route("/")
 def index():
@@ -23,9 +48,9 @@ def index():
 def health():
     return Response("OK", status=200)
 
-@app.route("/code-exec", methods=['POST'])
+@app.route("/api/v0/code-exec", methods=['POST'])
 def code_exec():
-    payload = json.loads(request.data["payload"])
+    payload = json.loads(request.form["payload"])
     globals_dict = deepcopy(payload["globals_dict"])
 
     unsafely = payload["unsafely"]
@@ -36,7 +61,7 @@ def code_exec():
 
     try:
         python_path=payload["python_path"]
-        extra_files=[(python_path[0], request.data[python_path[0]].read())]
+        extra_files=[(python_path[0], request.files[python_path[0]].read())]
 
         exec_fn(
             payload["code"],
@@ -59,4 +84,4 @@ def code_exec():
         "globals_dict": globals_dict,
         "emsg": emsg
     }
-    return JsonResponse(response, status=status.HTTP_200_OK, safe=False)
+    return jsonify(response)
