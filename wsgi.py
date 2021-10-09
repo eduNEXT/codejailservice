987dd0d6d15f862144e280dd868485c12392d413
Eric Herrera <eric.herrera@edunext.co>
2021-10-08 22:30:40 -0500

    fix: Add application for uwsgi.

--- wsgi.py
+++ wsgi.py
@@ -6,3 +6,5 @@ if __name__ == "__main__":
     host = os.getenv("FLASK_CODEJAILSERVICE_HOST", "0.0.0.0")
     port = os.getenv("FLASK_CODEJAILSERVICE_PORT", 8000)
     app.run(host=host, port=port)
+
+application = app
