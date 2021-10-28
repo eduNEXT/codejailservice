55ac8f4821db245ec961b0e4b515b0c3fc9bdca8
Johan Castiblanco <johancastiblanco17@gmail.com>
2021-10-28 18:01:53 -0500

    clean: fix whitespace in pyfiles.
    Fix some whitespace in `codejailservice/*`. Some pep 8 issues.

--- codejailservice/app.py
+++ codejailservice/app.py
@@ -11,43 +11,44 @@ from flask import Flask, Response, jsonify, request
 from logging.config import dictConfig
 
 
-dictConfig({
-    'version': 1,
-    'formatters': {
-        'default': {
-            'format': '%(asctime)s %(levelname)s %(process)d ' '[%(name)s] %(filename)s:%(lineno)d - %(message)s',
-        }
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'stream': sys.stdout,
-            'formatter': 'default'
-        }
-    },
-    'root': {
-        'level': 'DEBUG',
-        'handlers': ['console']
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "%(asctime)s %(levelname)s %(process)d "
+                "[%(name)s] %(filename)s:%(lineno)d - %(message)s",
+            }
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": sys.stdout,
+                "formatter": "default",
+            }
+        },
+        "root": {"level": "DEBUG", "handlers": ["console"]},
     }
-})
+)
 
 app = Flask(__name__)
 env_config = os.getenv("FLASK_APP_SETTINGS", "codejailservice.config.ProductionConfig")
 app.config.from_object(env_config)
 
+
 def configure_codejail(app):
     code_jail_settings = app.config["CODE_JAIL"]
-    python_bin = code_jail_settings.get('python_bin')
+    python_bin = code_jail_settings.get("python_bin")
     if python_bin:
-        user = code_jail_settings['user']
+        user = code_jail_settings["user"]
         jail_code.configure("python", python_bin, user=user)
-    limits = code_jail_settings.get('limits', {})
+    limits = code_jail_settings.get("limits", {})
     for name, value in limits.items():
         jail_code.set_limit(
             limit_name=name,
             value=value,
         )
-    limit_overrides = code_jail_settings.get('limit_overrides', {})
+    limit_overrides = code_jail_settings.get("limit_overrides", {})
     for context, overrides in limit_overrides.items():
         for name, value in overrides.items():
             jail_code.override_limit(
@@ -56,19 +57,23 @@ def configure_codejail(app):
                 limit_overrides_context=context,
             )
 
+
 configure_codejail(app)
 
 from codejail.safe_exec import SafeExecException, json_safe
 from codejail.safe_exec import not_safe_exec as codejail_not_safe_exec
 from codejail.safe_exec import safe_exec as codejail_safe_exec
 
+
 @app.route("/")
 def index():
     return Response("Edx Codejail Service", status=200)
 
+
 @app.route("/health")
 def health():
     return Response("OK", status=200)
+
 
 @app.post("/api/v0/code-exec")
 def code_exec():
@@ -82,14 +87,18 @@ def code_exec():
         exec_fn = codejail_safe_exec
 
     try:
-        python_path=payload["python_path"]
+        python_path = payload["python_path"]
         if python_path:
-            extra_files=[(python_path[0], request.files[python_path[0]].read())]
+            extra_files = [(python_path[0], request.files[python_path[0]].read())]
         else:
-            extra_files=[]
+            extra_files = []
         course_id = payload["limit_overrides_context"]
         problem_id = payload["slug"]
-        app.logger.info("Running problem_id:%s jailed code for course_id:%s ...", problem_id, course_id)
+        app.logger.info(
+            "Running problem_id:%s jailed code for course_id:%s ...",
+            problem_id,
+            course_id,
+        )
         start = timeit.default_timer()
         exec_fn(
             payload["code"],
@@ -107,13 +116,10 @@ def code_exec():
         exception = e
         emsg = str(e)
     else:
-        app.logger.info("Jailed code was executed in %s seconds.", str(end-start))
+        app.logger.info("Jailed code was executed in %s seconds.", str(end - start))
         exception = None
         emsg = None
 
-    response = {
-        "globals_dict": globals_dict,
-        "emsg": emsg
-    }
+    response = {"globals_dict": globals_dict, "emsg": emsg}
 
     return jsonify(response)
--- codejailservice/config.py
+++ codejailservice/config.py
@@ -1,42 +1,43 @@
 import os
 
+
 class BaseConfig:
     DEBUG = False
     DEVELOPMENT = False
     SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
 
     CODE_JAIL = {
-        'python_bin': '/sandbox/venv/bin/python',
+        "python_bin": "/sandbox/venv/bin/python",
         # User to run as in the sandbox.
-        'user': 'sandbox',
-
+        "user": "sandbox",
         # Configurable limits.
         # Setting all of them to 0 to disable limits in containers.
-        'limits': {
+        "limits": {
             #
-            'NPROC': 0,
+            "NPROC": 0,
             # How many CPU seconds can jailed code use?
-            'CPU': 0,
+            "CPU": 0,
             # Limit the memory of the jailed process to something high but not
             # infinite (512MiB in bytes)
-            'VMEM': 0,
+            "VMEM": 0,
             # Time in seconds that the jailed process has to run.
-            'REALTIME': 0,
+            "REALTIME": 0,
             # Needs to be non-zero so that jailed code can use it as their temp directory.(10MiB in bytes)
-            'FSIZE': 10485760,
+            "FSIZE": 10485760,
             # Disable usage of proxy (force thread-safe)
-            'PROXY': 0,
+            "PROXY": 0,
         },
-
         # Overrides to default configurable 'limits' (above).
         # Keys should be course run ids.
         # Values should be dictionaries that look like 'limits'.
         "limit_overrides": {},
     }
 
+
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
     DEVELOPMENT = True
+
 
 class ProductionConfig(BaseConfig):
     pass
