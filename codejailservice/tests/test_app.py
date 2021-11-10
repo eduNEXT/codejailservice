2719f4f3018f5d6025529590ae17001c260dce33
Johan Castiblanco <johancastiblanco17@gmail.com>
2021-11-10 10:12:29 -0500

    feat: add test for main and health route.

--- codejailservice/tests/test_app.py
+++ codejailservice/tests/test_app.py
@@ -7,6 +7,22 @@ import subprocess
 import pytest
 import requests
 
+APP_ROUTE = "http://localhost:8550"
+
 
 def test_works():
     assert True
+
+
+def test_main_route():
+    """This run that the main route app is working and sending a text."""
+    response = requests.get(APP_ROUTE)
+    assert response.status_code == 200
+    assert isinstance(response.text, str)
+
+
+def test_health_route():
+    """This run that the health route  app is working and sending a text."""
+    response = requests.get(f"{APP_ROUTE}/health")
+    assert response.status_code == 200
+    assert isinstance(response.text, str)
