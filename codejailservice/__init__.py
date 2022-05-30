ef6c655ab814b85287f157167b74f52ad7f34df7
Maria Grimaldi <maria.grimaldi@edunext.co>
2022-05-30 14:47:17 -0400

    Bump version: 13.0.0 → 14.0.0

--- codejailservice/__init__.py
+++ codejailservice/__init__.py
@@ -1,4 +1,4 @@
 """Allows for the remote execution of unsafe code via a rest API that runs restricted by an apparmor profile."""
 from .app import app
 
-__version__ = "13.0.0"
+__version__ = "14.0.0"
